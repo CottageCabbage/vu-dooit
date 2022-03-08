a339eddc64d8b33e568aa54d304fadb81b32d011
CottageCabbage <96266734+CottageCabbage@users.noreply.github.com>
2022-03-08 16:05:10 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view />
+  <Sidebar />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Sidebar from "./components/Sidebar.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Sidebar,
+  },
+});
+</script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Hello</h1>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Sidebar",
+});
+</script>
