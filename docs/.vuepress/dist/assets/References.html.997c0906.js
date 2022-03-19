6cb3b97e6a566454df822a5a9f6ea74c7b6139b5
CottageCabbage <96266734+CottageCabbage@users.noreply.github.com>
2022-03-19 08:11:20 -0300

    Forgot to build docs

---
+++ docs/.vuepress/dist/assets/References.html.997c0906.js
@@ -0,0 +1,1 @@
+const e={key:"v-04603f18",path:"/References.html",title:"References",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1647688087e3,contributors:[{name:"CottageCabbage",email:"96266734+CottageCabbage@users.noreply.github.com",commits:1}]},filePathRelative:"References.md"};export{e as data};
