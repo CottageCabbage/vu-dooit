f81c2e09f62e8c180d2dff35417ef6e91f17254e
CottageCabbage <96266734+CottageCabbage@users.noreply.github.com>
2022-04-16 11:28:27 -0300

    Added inputs to profile settings tab. Button and dialog

--- src/components/Dialogs/settingsDialog.vue
+++ src/components/Dialogs/settingsDialog.vue
@@ -5,10 +5,10 @@
     style="height: 525px; width: 800px !important; max-width: none"
     class="bg-grey-3"
   >
-    <q-splitter v-model="splitterModel" style="height: 525px">
+    <q-splitter v-model="splitterModel" style="height: 525px" disable>
       <template v-slot:before>
         <q-tabs v-model="tab" vertical class="bg-grey-4 text-light-green-10">
-          <q-tab name="mails" icon="manage_accounts" label="Profile" />
+          <q-tab name="profile" icon="manage_accounts" label="Profile" />
           <q-tab name="alarms" icon="palette" label="Appearance" />
           <q-tab name="movies" icon="movie" label="Movies" />
         </q-tabs>
@@ -18,21 +18,42 @@
         <q-tab-panels
           v-model="tab"
           animated
-          swipeable
           vertical
           transition-prev="jump-up"
           transition-next="jump-up"
           class="bg-grey-3"
         >
-          <q-tab-panel name="mails">
-            <div class="text-h4 q-mb-md">Profile Settings</div>
-            <form>
-              Username:
-              <input type="text" />
-              <br />
-              Bio:
-              <input type="text" />
-            </form>
+          <q-tab-panel name="profile">
+            <div class="text-h5 text-center q-mb-md">Profile Settings</div>
+            <q-form
+              @submit.prevent="saveChangesToProfile"
+              class="flexColumnContainer"
+              style="gap: 15px"
+            >
+              <q-input
+                outlined
+                v-model="text"
+                label="Username"
+                maxlength="15"
+              />
+              <q-input outlined v-model="text" label="Bio" type="textarea" />
+              <q-btn label="Submit" type="submit" color="green" />
+            </q-form>
+
+            <q-banner class="bg-green text-white" v-if="showBanner">
+              <div class="row">
+                <q-space />
+                <span style="font-size: 1.2rem">Changes to profile saved!</span>
+                <q-space />
+                <q-btn
+                  flat
+                  color="white"
+                  label="Dismiss"
+                  style="margin-left: auto"
+                  @click="showBanner = false"
+                />
+              </div>
+            </q-banner>
           </q-tab-panel>
 
           <q-tab-panel name="alarms">
@@ -86,7 +107,26 @@ export default defineComponent({
   setup() {
     return {
       tab: ref('mails'),
+      showBanner: ref(false),
     };
+  },
+  methods: {
+    saveChangesToProfile() {
+      console.log('hello');
+      this.showBanner = true;
+    },
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.q-banner {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+
+  > .q-banner__content {
+  }
+}
+</style>
